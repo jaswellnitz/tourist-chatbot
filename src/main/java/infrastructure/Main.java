d375d2b38eb2db2cd18d798bb0385b2d2c868019
Jasmin Wellnitz <jasmin.wellnitz@haw-hamburg.de>
2017-03-04 20:06:04 +0100

    changed to current java spark version

--- src/main/java/infrastructure/Main.java
+++ src/main/java/infrastructure/Main.java
@@ -3,57 +3,39 @@ package infrastructure;
 import static spark.Spark.after;
 import static spark.Spark.post;
 import static spark.Spark.get;
-import static spark.Spark.setPort;
+import static spark.Spark.port;
 
-import javax.annotation.PostConstruct;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import com.google.gson.JsonParser;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import spark.ResponseTransformer;
 
 public class Main {
 
 	 public static void main(String[] args) {
-		 setPort(Integer.valueOf(System.getenv("PORT")));
+		 port(Integer.valueOf(System.getenv("PORT")));
 		 
-		 get(new Route("/hello") {
-             @Override
-             public Object handle(Request request, Response response) { 
-    			return "Hello World!";
-             }
- });	 
+
+	     get("/hello", (req, res) -> "Hello World");
+		
 		 
-		 post(new Route("/test") {
-             @Override
-             public Object handle(Request request, Response response) {
+		 post("/test",(req,res)-> {
      			Fulfillment fulfillment = new Fulfillment("Hello Speech", "Hello Display Test!", "touristbot test");
      			return JsonUtil.toJson(fulfillment);
              }
- });	
-		
-		 post(new Route("/webhook") {
-             @Override
-             public Object handle(Request request, Response response) {
-//     			String jsonRequest = request.body().toString();
+ );	
+		 
+		 post("/webhook",(req,res)-> {
+//				String jsonRequest = request.body().toString();
 //     			JsonObject result = new JsonParser().parse(jsonRequest).getAsJsonObject().get("result").getAsJsonObject();
 //     			String action = result.get("action").getAsString();
-     			Fulfillment fulfillment = new Fulfillment("Hello Speech", "Hello Display Test!", "touristbot test");
-     			return JsonUtil.toJson(fulfillment);
-             }
- });	 
-		after(new spark.Filter() {
-			
-			@Override
-			public void handle(Request req, Response res) {
+  			Fulfillment fulfillment = new Fulfillment("Hello Speech", "Hello Display Test!", "touristbot test");
+  			return JsonUtil.toJson(fulfillment);
+          }
+);	
+		
+		 after((req, res) -> {
 				res.type("application/json");
-				
-			}
-		});
-	 
+	});
 	 }
 	 
 }
