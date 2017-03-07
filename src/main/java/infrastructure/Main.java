3f57853f5008918ebb02c3462d644a747f64bbb7
Jasmin Wellnitz <jasmin.wellnitz@haw-hamburg.de>
2017-03-07 13:55:19 +0100

    moved setup data to config file and parse telegram user data

--- src/main/java/infrastructure/Main.java
+++ src/main/java/infrastructure/Main.java
@@ -1,6 +1,11 @@
 package infrastructure;
 
 import static spark.Spark.post;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.*;
@@ -12,7 +17,7 @@ import com.pengrad.telegrambot.request.SetWebhook;
 public class Main {
 
 	public static void main(String[] args) {
-			port(Integer.valueOf(System.getenv("PORT")));
+//			port(Integer.valueOf(System.getenv("PORT")));
 
 	        get("/hello", (req, res) -> "Hello World");
 	        post("/test", (req, res) -> "Post Test");
@@ -21,6 +26,14 @@ public class Main {
 	        TestBotHandler botHandler = new TestBotHandler();
 	        post("/" + botHandler.getToken(), botHandler);
 
-	        botHandler.getBot().execute(new SetWebhook().url("https://safe-ridge-54902.herokuapp.com/" + botHandler.getToken()));
+	        Properties properties = new Properties();
+	         try {
+				properties.load(new FileInputStream("config.properties"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	         System.out.println(properties.get("serviceurl"));
+	         
+	        botHandler.getBot().execute(new SetWebhook().url(properties.getProperty("serviceurl") + botHandler.getToken()));
 	}
 }
