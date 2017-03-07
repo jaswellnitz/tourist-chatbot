34ad9619e4d503dff000086763f802565fce6f50
Jasmin Wellnitz <jasmin.wellnitz@haw-hamburg.de>
2017-03-07 18:33:00 +0100

    added http connection to api.ai

--- src/main/java/util/JsonUtil.java
+++ src/main/java/util/JsonUtil.java
@@ -1,6 +1,8 @@
-package infrastructure;
+package util;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import spark.ResponseTransformer;
 
@@ -9,6 +11,12 @@ public class JsonUtil {
 	public static String toJson(Object object) {
 		return new Gson().toJson(object);
 	}
+	
+	public static JsonObject parseToJson(String text){
+		return new JsonParser().parse(text).getAsJsonObject();
+	}
+	
+	
 
 	public static ResponseTransformer json() {
 		return JsonUtil::toJson;
